9412ba60e4ae93f6366add9dee006af86c96bcb9
Philipp Hamerle <phamerle.mmt-b2016@fh-salzburg.ac.at>
2018-05-14 16:29:40 +0200

    fix: select shows selected track & buttons disable

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ h1 {
 `;
 
 const wagon = 'railway_car';
-const train = 'steam_locomotive'
+const train = 'steam_locomotive';
 
 app.model({
 state: {
@@ -78,41 +78,58 @@ reducers: {
 }
 });
 
-const mainView = (state, prev, send) => html`
-<main class=${styles}>
-    <h1>🚂🚃 Trainstation 🚂🚃</h1>
-    <hr>
-    <div>
-    🚉 Loks: ${state.trains.map((v) => emoji.get(v))}
-    </div>
-    <div>
-    🚉 Pool: ${state.wagons.map((v) => emoji.get(v))}
-    </div>
-    <hr>
-    <button onclick=${() =>
-    send('addWagon')} class="btn btn-primary">Add Wagon</button>
-    <button onclick=${() =>
-    send('moveWagon', {track: 'A'})} class="btn btn-danger">Add to A</button>
-    <button onclick=${() =>
-    send('moveWagon', {track: 'B'})} class="btn btn-danger">Add to B</button>
-    <select onchange=${() => {
-        let e = document.getElementById("selectTrack")
-        send('changeSelected', {track: e.options[e.selectedIndex].value})}
-    } id="selectTrack">   
-        <option value="A">Track A</option>
-        <option value="B">Track B</option>
-    </select>
-    <button onclick=${() =>
-        send('scheduleTrain', {track: state.selected})} class="btn btn-success">Schedule</button>
-    <hr>
-    <div class="gleis">
-    🛤️ Track A: ${state.trackA.map((v) => emoji.get(v))}
-    </div>
-    <div class="gleis">
-    🛤️ Track B: ${state.trackB.map((v) => emoji.get(v))}
-    </div>
-</main>
-`;
+const mainView = (state, prev, send) => {
+
+    const poolFull = state.wagons.length >= 10;
+    const trackAFull = state.trackA.length >= 5;
+    const trackBFull = state.trackB.length >= 6;
+    const poolEmpty = state.wagons.length == 0;
+    const trainsEmpty = state.trains.length == 0;
+    const schedulable = (state.selected == 'A' && state.trackA.length > 0) || (state.selected == 'B' && state.trackB.length > 0);
+
+    return html`
+    <main class=${styles}>
+        <h1>🚂🚃 Trainstation 🚂🚃</h1>
+        <hr>
+        <div>
+        🚉 Loks: ${state.trains.map((v) => emoji.get(v))}
+        </div>
+        <div>
+        🚉 Pool: ${state.wagons.map((v) => emoji.get(v))}
+        </div>
+        <hr>
+        <button ${
+            poolFull ? "disabled" : ""
+            } onclick=${() =>
+            send('addWagon')} class="btn btn-primary">Add Wagon</button>
+            <button ${
+                trackAFull || poolEmpty || trainsEmpty ? "disabled" : ""
+            } onclick=${() =>
+            send('moveWagon', {track: 'A'})} class="btn btn-danger">Add to A</button>
+            <button ${
+                trackBFull || poolEmpty || trainsEmpty ? "disabled" : ""
+            } onclick=${() =>
+            send('moveWagon', {track: 'B'})} class="btn btn-danger">Add to B</button>
+            <select onchange=${(el) => {
+                send('changeSelected', {track: el.target.value})}
+            } id="selectTrack">   
+                <option ${state.selected == 'A' ? 'selected' : ''} value="A">Track A</option>
+                <option ${state.selected == 'B' ? 'selected' : ''} value="B">Track B</option>
+            </select>
+            <button ${
+                !schedulable ? "disabled" : ""
+            } onclick=${() =>
+                send('scheduleTrain', {track: state.selected})} class="btn btn-success">Schedule</button>
+            <hr>
+            <div class="gleis">
+            🛤️ Track A: ${state.trackA.map((v) => emoji.get(v))}
+            </div>
+            <div class="gleis">
+            🛤️ Track B: ${state.trackB.map((v) => emoji.get(v))}
+            </div>
+    </main>
+    `;
+}
 
 app.router((route) => [route('/', mainView)]);
 
